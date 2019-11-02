939b732d4cc8270910486a14fcc342a9a98d3117
IT-Kubat <kubat88@list.ru>
2019-11-02 12:38:08 +0600

    Homework 3.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] names = new String[3];
+        names[0] = "Erbol";
+        names[1] = "Nurbek";
+        names[2] = "Marina";
+        System.out.println(names[0] + "  " + names[1] + "  " + names[2]);
+        System.out.println("Доброе утро" + "   " + names[0]);
+        System.out.println("Добрый день" + "   " + names[1]);
+        System.out.println("Добрый вечер" + "   " + names[2]);
+        System.out.println("Здравствуйте" + "   " + names[0] + "   " + names[1] + "   "+ names[2]);
+
+
+
+    }
+}
